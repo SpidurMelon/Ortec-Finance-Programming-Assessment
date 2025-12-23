b068ceae18f5a7e29973bfb259e2ab54e948040a
SpidurMelon <julianvdweijden@gmail.com>
2025-12-23 09:54:23 +0100

    Extracted command line logic
    Split command line logic away from core logic.
    This is to simplify code, enable easier testing and to increase extensibility.

--- src/main/java/com/ortecfinance/tasklist/TaskList.java
+++ src/main/java/com/ortecfinance/tasklist/TaskList.java
@@ -4,118 +4,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public final class TaskList implements Runnable {
-    private static final String QUIT = "quit";
+public final class TaskList {
+    public static final class ProjectNotFoundException extends Exception {
+        public ProjectNotFoundException(String project) {
+            super("Could not find a project with the name \"%s\".".formatted(project));
+        }
+    }
+    public static final class TaskNotFoundException extends Exception {
+        public TaskNotFoundException(int id) {
+            super("Could not find a task with an ID of %d.".formatted(id));
+        }
+    }
 
     private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
-    private final BufferedReader in;
-    private final PrintWriter out;
-
     private long lastId = 0;
 
-    public static void startConsole() {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        new TaskList(in, out).run();
-    }
-
-    public TaskList(BufferedReader reader, PrintWriter writer) {
-        this.in = reader;
-        this.out = writer;
-    }
-
-    public void run() {
-        out.println("Welcome to TaskList! Type 'help' for available commands.");
-        while (true) {
-            out.print("> ");
-            out.flush();
-            String command;
-            try {
-                command = in.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            if (command.equals(QUIT)) {
-                break;
-            }
-            execute(command);
-        }
-    }
-
-    private void execute(String commandLine) {
-        String[] commandRest = commandLine.split(" ", 2);
-        String command = commandRest[0];
-        switch (command) {
-            case "show":
-                show();
-                break;
-            case "add":
-                add(commandRest[1]);
-                break;
-            case "check":
-                check(commandRest[1]);
-                break;
-            case "uncheck":
-                uncheck(commandRest[1]);
-                break;
-            case "help":
-                help();
-                break;
-            default:
-                error(command);
-                break;
-        }
-    }
-
-    private void show() {
-        for (Map.Entry<String, List<Task>> project : tasks.entrySet()) {
-            out.println(project.getKey());
-            for (Task task : project.getValue()) {
-                out.printf("    [%c] %d: %s%n", (task.isDone() ? 'x' : ' '), task.getId(), task.getDescription());
-            }
-            out.println();
-        }
-    }
-
-    private void add(String commandLine) {
-        String[] subcommandRest = commandLine.split(" ", 2);
-        String subcommand = subcommandRest[0];
-        if (subcommand.equals("project")) {
-            addProject(subcommandRest[1]);
-        } else if (subcommand.equals("task")) {
-            String[] projectTask = subcommandRest[1].split(" ", 2);
-            addTask(projectTask[0], projectTask[1]);
-        }
-    }
-
-    private void addProject(String name) {
+    public void addProject(String name) {
         tasks.put(name, new ArrayList<Task>());
     }
 
-    private void addTask(String project, String description) {
+    public void addTask(String project, String description) throws ProjectNotFoundException {
+        if (!tasks.containsKey(project)) throw new ProjectNotFoundException(project);
         List<Task> projectTasks = tasks.get(project);
-        if (projectTasks == null) {
-            out.printf("Could not find a project with the name \"%s\".", project);
-            out.println();
-            return;
-        }
         projectTasks.add(new Task(nextId(), description, false));
     }
 
-    private void check(String idString) {
+    public Set<Map.Entry<String, List<Task>>> getProjects() {
+        return tasks.entrySet();
+    }
+
+    public Set<String> getProjectNames() {
+        return tasks.keySet();
+    }
+
+    public List<Task> getTasks(String project) throws ProjectNotFoundException {
+        if (!tasks.containsKey(project)) throw new ProjectNotFoundException(project);
+        return tasks.get(project);
+    }
+
+    public void check(String idString) throws TaskNotFoundException {
         setDone(idString, true);
     }
 
-    private void uncheck(String idString) {
+    public void uncheck(String idString) throws TaskNotFoundException {
         setDone(idString, false);
     }
 
-    private void setDone(String idString, boolean done) {
+    private void setDone(String idString, boolean done) throws TaskNotFoundException {
         int id = Integer.parseInt(idString);
         for (Map.Entry<String, List<Task>> project : tasks.entrySet()) {
             for (Task task : project.getValue()) {
@@ -125,23 +62,7 @@ public final class TaskList implements Runnable {
                 }
             }
         }
-        out.printf("Could not find a task with an ID of %d.", id);
-        out.println();
-    }
-
-    private void help() {
-        out.println("Commands:");
-        out.println("  show");
-        out.println("  add project <project name>");
-        out.println("  add task <project name> <task description>");
-        out.println("  check <task ID>");
-        out.println("  uncheck <task ID>");
-        out.println();
-    }
-
-    private void error(String command) {
-        out.printf("I don't know what the command \"%s\" is.", command);
-        out.println();
+        throw new TaskNotFoundException(id);
     }
 
     private long nextId() {
---
+++ src/main/java/com/ortecfinance/tasklist/TaskListCLI.java
@@ -0,0 +1,133 @@
+package com.ortecfinance.tasklist;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+public class TaskListCLI implements Runnable {
+    private static final String QUIT = "quit";
+
+    private final BufferedReader in;
+    private final PrintWriter out;
+
+    private final TaskList taskList;
+
+    public TaskListCLI(BufferedReader reader, PrintWriter writer, TaskList taskList) {
+        this.in = reader;
+        this.out = writer;
+        this.taskList = taskList;
+    }
+
+    public TaskListCLI(BufferedReader reader, PrintWriter writer) {
+        this(
+                reader,
+                writer,
+                new TaskList()
+        );
+    }
+
+    public TaskListCLI() {
+        this(
+                new BufferedReader(new InputStreamReader(System.in)),
+                new PrintWriter(System.out)
+        );
+    }
+
+    public static void startConsole() {
+        new TaskListCLI().run();
+    }
+
+    public void run() {
+        out.println("Welcome to TaskList! Type 'help' for available commands.");
+        while (true) {
+            out.print("> ");
+            out.flush();
+            String command;
+            try {
+                command = in.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if (command.equals(QUIT)) {
+                break;
+            }
+            execute(command);
+        }
+    }
+
+    private void execute(String commandLine) {
+        String[] commandRest = commandLine.split(" ", 2);
+        String command = commandRest[0];
+        switch (command) {
+            case "show":
+                show();
+                break;
+            case "add":
+                add(commandRest[1]);
+                break;
+            case "check":
+                try {
+                    taskList.check(commandRest[1]);
+                } catch (TaskList.TaskNotFoundException e) {
+                    out.println(e.getMessage());
+                }
+                break;
+            case "uncheck":
+                try {
+                    taskList.uncheck(commandRest[1]);
+                } catch (TaskList.TaskNotFoundException e) {
+                    out.println(e.getMessage());
+                }
+                break;
+            case "help":
+                help();
+                break;
+            default:
+                error(command);
+                break;
+        }
+    }
+
+    private void add(String commandLine) {
+        String[] subcommandRest = commandLine.split(" ", 2);
+        String subcommand = subcommandRest[0];
+        if (subcommand.equals("project")) {
+            taskList.addProject(subcommandRest[1]);
+        } else if (subcommand.equals("task")) {
+            String[] projectTask = subcommandRest[1].split(" ", 2);
+            try {
+                taskList.addTask(projectTask[0], projectTask[1]);
+            } catch (TaskList.ProjectNotFoundException e) {
+                out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void show() {
+        for (Map.Entry<String, List<Task>> project : taskList.getProjects()) {
+            out.println(project.getKey());
+            for (Task task : project.getValue()) {
+                out.printf("    [%c] %d: %s%n", (task.isDone() ? 'x' : ' '), task.getId(), task.getDescription());
+            }
+            out.println();
+        }
+    }
+
+    private void help() {
+        out.println("Commands:");
+        out.println("  show");
+        out.println("  add project <project name>");
+        out.println("  add task <project name> <task description>");
+        out.println("  check <task ID>");
+        out.println("  uncheck <task ID>");
+        out.println();
+    }
+
+    private void error(String command) {
+        out.printf("I don't know what the command \"%s\" is.", command);
+        out.println();
+    }
+}
